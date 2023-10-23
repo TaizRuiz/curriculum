d5c82c602fdaaffbc6fe297447d89114c36593cf
TaizRuiz <dafnetaizruiz@gmail.com>
2023-10-22 21:35:33 -0500

    curriculum

---
+++ dafneRuizCurriculum/index.js
@@ -0,0 +1,20 @@
+function removeEffect(linkName){
+    let href=linkName.getAttribute("href");
+    let section=href.substring(1);
+    document.getElementById(section).classList.remove('activate-float');
+    linkName.classList.remove('link-active');
+}
+const links=document.querySelectorAll("nav ul li a");
+links.forEach(link=>{
+    link.addEventListener("click",()=>{
+        links.forEach(link => {
+            removeEffect(link);
+        });
+        link.classList.toggle("link-active");
+        let href_current=link.getAttribute("href");
+        let section_current=href_current.substring(1);
+        document.getElementById(section_current).classList.toggle('activate-float');
+    });
+});
+
+
